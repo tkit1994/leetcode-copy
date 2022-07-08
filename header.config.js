a0080bea1d0d547841d238f6efbfa73c769b807b
HaoCheng <ch1994@outlook.com>
2022-07-08 18:22:38 +0800

    update

--- header.config.js
+++ header.config.js
@@ -3,7 +3,7 @@ const { defineTmHeader } = require('vite-plugin-tm-userscript')
 module.exports = defineTmHeader({
 	name: 'LeetcodeCopy',
 	namespace: 'com.github.tkit1994',
-	version: '1.0.0',
+	version: '1.0.1',
 	author: 'HaoCheng',
 	description: "Add a copy button in leetcode.cn's editor to copy code in vim mode",
 	homepage: 'https://github.com/tkit1994/leetcode-copy',
